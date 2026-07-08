e243b76fdf8a88cebbb681a888afadd562632110
Administrator <administrator@local>
2026-07-08 14:04:26 +0800

    关卡配置先行

---
+++ src/ReplicatedStorage/theta/Gameplay/StageTheta.lua
@@ -0,0 +1,159 @@
+-- StageTheta
+-- 关卡玩法配置。场景路径和触发逻辑后续单独放到 Scene / Transition 层。
+
+local StageTheta = {
+	Stages = {
+		[1] = {
+			StageId = 1,
+			RecommendedStrength = 45,
+			RewardTrophies = 1,
+		},
+		[2] = {
+			StageId = 2,
+			RecommendedStrength = 650,
+			RewardTrophies = 5,
+		},
+		[3] = {
+			StageId = 3,
+			RecommendedStrength = 5000,
+			RewardTrophies = 15,
+		},
+		[4] = {
+			StageId = 4,
+			RecommendedStrength = 30000,
+			RewardTrophies = 40,
+		},
+		[5] = {
+			StageId = 5,
+			RecommendedStrength = 95000,
+			RewardTrophies = 100,
+		},
+		[6] = {
+			StageId = 6,
+			RecommendedStrength = 450000,
+			RewardTrophies = 300,
+		},
+		[7] = {
+			StageId = 7,
+			RecommendedStrength = 1000000,
+			RewardTrophies = 750,
+		},
+		[8] = {
+			StageId = 8,
+			RecommendedStrength = 7500000,
+			RewardTrophies = 2250,
+		},
+		[9] = {
+			StageId = 9,
+			RecommendedStrength = 25000000,
+			RewardTrophies = 7500,
+		},
+		[10] = {
+			StageId = 10,
+			RecommendedStrength = 111000000,
+			RewardTrophies = 42500,
+		},
+		[11] = {
+			StageId = 11,
+			RecommendedStrength = 450000000,
+			RewardTrophies = 105000,
+		},
+		[12] = {
+			StageId = 12,
+			RecommendedStrength = 1000000000,
+			RewardTrophies = 310000,
+		},
+		[13] = {
+			StageId = 13,
+			RecommendedStrength = 3500000000,
+			RewardTrophies = 750000,
+		},
+		[14] = {
+			StageId = 14,
+			RecommendedStrength = 25000000000,
+			RewardTrophies = 2500000,
+		},
+		[15] = {
+			StageId = 15,
+			RecommendedStrength = 75000000000,
+			RewardTrophies = 8500000,
+		},
+		[16] = {
+			StageId = 16,
+			RecommendedStrength = "1.75E+11",
+			RewardTrophies = 25000000,
+		},
+		[17] = {
+			StageId = 17,
+			RecommendedStrength = "4.20E+11",
+			RewardTrophies = 75000000,
+		},
+		[18] = {
+			StageId = 18,
+			RecommendedStrength = "1.50E+12",
+			RewardTrophies = 200000000,
+		},
+		[19] = {
+			StageId = 19,
+			RecommendedStrength = "1.20E+13",
+			RewardTrophies = 750000000,
+		},
+		[20] = {
+			StageId = 20,
+			RecommendedStrength = "6.70E+13",
+			RewardTrophies = 2500000000,
+		},
+		[21] = {
+			StageId = 21,
+			RecommendedStrength = "2.25E+14",
+			RewardTrophies = 4000000000,
+		},
+		[22] = {
+			StageId = 22,
+			RecommendedStrength = "1.50E+15",
+			RewardTrophies = 12500000000,
+		},
+		[23] = {
+			StageId = 23,
+			RecommendedStrength = "4.00E+15",
+			RewardTrophies = 37000000000,
+		},
+		[24] = {
+			StageId = 24,
+			RecommendedStrength = "1.50E+16",
+			RewardTrophies = "1.00E+11",
+		},
+		[25] = {
+			StageId = 25,
+			RecommendedStrength = "7.90E+16",
+			RewardTrophies = "3.50E+11",
+		},
+		[26] = {
+			StageId = 26,
+			RecommendedStrength = "2.06E+17",
+			RewardTrophies = "7.00E+11",
+		},
+		[27] = {
+			StageId = 27,
+			RecommendedStrength = "6.25E+17",
+			RewardTrophies = "2.50E+12",
+		},
+		[28] = {
+			StageId = 28,
+			RecommendedStrength = "1.50E+18",
+			RewardTrophies = "9.00E+12",
+		},
+		[29] = {
+			StageId = 29,
+			RecommendedStrength = "5.00E+18",
+			RewardTrophies = "3.10E+13",
+		},
+		[30] = {
+			StageId = 30,
+			RecommendedStrength = "2.50E+19",
+			RewardTrophies = "1.05E+14",
+		},
+	},
+}
+
+return StageTheta
